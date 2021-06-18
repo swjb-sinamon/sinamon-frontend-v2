eb90187a9b57756938839eb5dd9594b5f76a1010
DaegyeomHa <combbm@naver.com>
2021-06-19 00:17:16 +0900

    chore(lint): add lint and style tool dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['react-app', 'airbnb', 'eslint-config-prettier'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'linebreak-style': 'off',
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    'no-use-before-define': 'off'
+  }
+};
