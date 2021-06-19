f13ea38226890ec84fd2f0ef3ea40f1e4b13b054
DaegyeomHa <combbm@naver.com>
2021-06-19 19:58:52 +0900

    chore: update lint and style config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,20 @@ module.exports = {
     'jsx-quotes': 'off',
     'comma-dangle': 'off',
     'object-curly-newline': 'off',
+
     'react/jsx-filename-extension': 'off',
     'react/no-unescaped-entities': 'off',
     'react/jsx-tag-spacing': 'off',
     'react/prop-types': 'off',
     'react/jsx-one-expression-per-line': 'off',
+
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+
     'no-console': 'off',
+
     'linebreak-style': 'off',
 
     'no-shadow': 'off',
