45121de09bb678daf974ed2a6c751d9d1f2ff667
DaegyeomHa <combbm@naver.com>
2021-06-20 16:38:55 +0900

    feat: create MainPage.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Switch>{/* TODO */}</Switch>
+      <Switch>
+        <Route path="/" component={MainPage} exact />
+      </Switch>
     </BrowserRouter>
   );
 };
