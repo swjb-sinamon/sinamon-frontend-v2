94094bedf39a577746a11ed7d4fe8ad49a5b51ea
DaegyeomHa <combbm@naver.com>
2021-06-23 13:42:38 +0900

    feat: add webfont

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -7,6 +7,12 @@ const GlobalStyle: React.FC = () => {
       styles={css`
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;700;900&display=swap');
         @import url('https://cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css');
+        @font-face {
+          font-family: 'paybooc-Bold';
+          src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/paybooc-Bold.woff') format('woff');
+          font-weight: normal;
+          font-style: normal;
+        }
 
         * {
           margin: 0;
