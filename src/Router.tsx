d17605a946a347a15b02b2908846f32119bb88b4
DaegyeomHa <combbm@naver.com>
2021-06-26 16:03:54 +0900

    feat: create PrivacyPage and ToSPage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import PermissionRoute from './utils/PermissionRoute';
 import LoginPage from './pages/LoginPage';
+import PrivacyPage from './pages/PrivacyPage';
+import ToSPage from './pages/ToSPage';
 
 const RedirectLogin = () => <Redirect to="/login" />;
 
@@ -12,6 +14,8 @@ const Router: React.FC = () => {
       <Switch>
         <PermissionRoute path="/" success={MainPage} failure={RedirectLogin} exact />
         <PermissionRoute path="/login" success={() => <Redirect to="/" />} failure={LoginPage} exact />
+        <Route path="/privacy" component={PrivacyPage} exact />
+        <Route path="/tos" component={ToSPage} exact />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/PrivacyPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivacyPage = () => {
+  return (
+    <>
+      <p>개인정보처리방침</p>
+    </>
+  );
+};
+
+export default PrivacyPage;
