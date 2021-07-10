19dea7cc37a1bf8e9b92400ab429a1549ccb643a
DaegyeomHa <combbm@naver.com>
2021-07-10 18:19:18 +0900

    fix: add sidebar contents

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendarWeek,
+  faChalkboard,
+  faSchool,
+  faSignOutAlt,
+  faStickyNote,
+  faUtensils
+} from '@fortawesome/free-solid-svg-icons';
 import SidebarTitle from './SidebarTitle';
 import SidebarItem from './SidebarItem';
 import { Breakpoints, makeMediaQuery } from '../../styles/Breakpoint';
@@ -32,9 +39,12 @@ const Sidebar: React.FC = () => {
       <SidebarTitle setOpen={setOpen} />
 
       <SidebarList open={isOpen}>
-        <SidebarItem icon={faStickyNote}>가나다</SidebarItem>
-        <SidebarItem icon={faStickyNote}>가나다</SidebarItem>
-        <SidebarItem icon={faStickyNote}>가나다</SidebarItem>
+        <SidebarItem icon={faUtensils}>급식</SidebarItem>
+        <SidebarItem icon={faCalendarWeek}>시간표</SidebarItem>
+        <SidebarItem icon={faChalkboard}>수강신청</SidebarItem>
+        <SidebarItem icon={faSchool}>학사일정</SidebarItem>
+        <SidebarItem icon={faStickyNote}>익명건의함</SidebarItem>
+        <SidebarItem icon={faSignOutAlt}>로그아웃</SidebarItem>
       </SidebarList>
     </StyledSidebar>
   );
