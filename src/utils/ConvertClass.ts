4dbb913b022c94fdb0f8cf136784b201a3886def
DaegyeomHa <combbm@naver.com>
2021-07-16 17:43:05 +0900

    fix: 파라미터 타입에 export 적용

--- src/utils/ConvertClass.ts
+++ src/utils/ConvertClass.ts
@@ -1,8 +1,8 @@
-type Department = 1 | 2 | 3 | 4 | 5;
-type Clazz = 1 | 2;
-type FullClass = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type DepartmentType = 1 | 2 | 3 | 4 | 5;
+export type ClazzType = 1 | 2;
+export type FullClassType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-export const convertClassToFullClass = (department: Department, clazz: Clazz): FullClass => {
+export const convertClassToFullClass = (department: DepartmentType, clazz: ClazzType): FullClassType => {
   if (department === 1 && clazz === 1) return 1;
   if (department === 1 && clazz === 2) return 2;
 
