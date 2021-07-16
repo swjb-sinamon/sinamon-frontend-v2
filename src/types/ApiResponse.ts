ce3d1beb6d2f09a2fcce70c47df0826584000db7
DaegyeomHa <combbm@naver.com>
2021-07-16 17:45:41 +0900

    feat: implement useTimetable hooks to get timetable data

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -25,3 +25,16 @@ export interface WeatherType {
   readonly status: Capitalize<string>;
   readonly temp: number;
 }
+
+export interface ComciganTimetableType {
+  readonly grade: number;
+  readonly class: number;
+  readonly weekday: number;
+  readonly weekdayString: string;
+  // eslint-disable-next-line camelcase
+  readonly class_time: number;
+  readonly code: string;
+  readonly teacher: string;
+  readonly subject: string;
+  readonly url: string;
+}
