4c0c0fb764d593bb64b6a9c617973942ce1d3d29
DaegyeomHa <combbm@naver.com>
2021-07-16 17:54:03 +0900

    fix: useMeal hooks를 통해 내일 급식을 가져오도록 수정

--- src/hooks/useMeal.tsx
+++ src/hooks/useMeal.tsx
@@ -1,15 +1,30 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import Api from '../apis';
 
-const DEFAULT_TEXT = '급식이 없습니다.';
-const context = createContext<string>(DEFAULT_TEXT);
+const DEFAULT_DATA = {
+  today: '급식이 없습니다.',
+  tomorrow: '급식이 없습니다.'
+};
+const context = createContext<Record<'today' | 'tomorrow', string>>(DEFAULT_DATA);
 
 export const MealProvider: React.FC = ({ children }) => {
-  const [meal, setMeal] = useState<string>(DEFAULT_TEXT);
+  const [meal, setMeal] = useState<Record<'today' | 'tomorrow', string>>(DEFAULT_DATA);
+
   useEffect(() => {
-    Api.get('/school/meal?type=today').then((res) => {
-      setMeal(res.data.data);
-    });
+    Api.get('/school/meal?type=today')
+      .then((res) => {
+        setMeal((current) => ({
+          ...current,
+          today: res.data.data
+        }));
+        return Api.get('/school/meal?type=tomorrow');
+      })
+      .then((res) => {
+        setMeal((current) => ({
+          ...current,
+          tomorrow: res.data.data
+        }));
+      });
   }, []);
 
   return <context.Provider value={meal}>{children}</context.Provider>;
