06adfee1f70417a4c56eecd33feca5a19d76fd5d
shc0801 <shc123987@naver.com>
2021-07-25 19:27:03 +0900

    fix: update Change icons according to weather conditions and Prittier

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -3,111 +3,121 @@ import styled from '@emotion/styled';
 import Notice from '../Notice/index';
 import { useWeather } from '../../hooks/useWeather';
 import { Heading2 } from '../../atoms/Typography/Heading';
+import { convertPm10ToString, convertPm25ToString } from '../../utils/Dust';
 import WeatherIcon from '../../atoms/Icon/WeatherIcon';
 
-console.log(WeatherIcon);
-
 const WeatherNoticeContainer = styled.div`
-    width: 380px;
+  width: 380px;
 
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 const WeatherHeader = styled.div`
-    margin-bottom: 8px;
-`
+  margin-bottom: 8px;
+`;
 
-// const StyledWeatherText = styled.span`
-//   color: var(--color-main);
-// `;
+const WeatherContent = styled.div<{ background: string }>`
+  background: ${(props) => props.background};
 
-const WeatherContent = styled.div<{ background: string[] }>`
-    background: linear-gradient(to top, #a8c0ff, #72a6d6);
+  border-radius: 30px;
 
-    border-radius: 30px;
+  padding: 45px 40px 30px;
+`;
 
-    padding: 45px 40px 30px;
-`
 const WeatherContentTop = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    padding: 0 30px 25px;
+  padding: 0 30px 25px;
 
-    border-bottom: 2px solid #fff; 
-`
+  border-bottom: 2px solid #fff;
+`;
 
 const WeatherContentStatus = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
 
 const StyledWeatherTemp = styled.div`
-    font-size: 3.4rem;
-    color: #fff;
-    
-    font-weight: 700;
-`
+  font-size: 3.4rem;
+  color: #fff;
+
+  font-weight: 700;
+`;
 
 const StyledWeatherStatus = styled.div`
-    color: #fff;
-    font-size: 1.2rem;
-`
+  color: #fff;
+  font-size: 1.2rem;
+`;
 
 const DustContentBottom = styled.div`
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 
-    padding: 12px 15px;
-`
+  padding: 12px 15px;
+`;
 
 const StyledDustStatus = styled.p`
-    color: #fff;
-`
+  color: #fff;
+`;
 
 const StyledDustHighlight = styled.span`
-    font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 const WeatherCard: React.FC = () => {
-    const { weather, dust } = useWeather();
-    
-    console.log(weather, dust);
+  const { weather, dust } = useWeather();
 
-    const background: Record<string, string[]> = {
-        CLEAR: ["#e0eafc", "#cfdef3"],
-        RAIN: ["#457fca", "#5691c8"]
-    }
+  console.log(weather, dust);
 
-    return (
-        <WeatherNoticeContainer>
-            <WeatherHeader>
-                <Heading2>
-                    오늘의 날씨
-                </Heading2>
-            </WeatherHeader>
-            <WeatherContent background={background.RAIN}>
-                <WeatherContentTop>
-                    <WeatherIcon weather="RAIN" />
-                    <WeatherContentStatus>
-                        <StyledWeatherTemp>
-                            {weather.temp}℃ 
-                        </StyledWeatherTemp>
-                        <StyledWeatherStatus>
-                            비
-                        </StyledWeatherStatus>
-                    </WeatherContentStatus>
-                </WeatherContentTop>
-                <DustContentBottom>
-                    <StyledDustStatus>미세먼지 <StyledDustHighlight>좋음</StyledDustHighlight></StyledDustStatus>
-                    <StyledDustStatus>초미세먼지 <StyledDustHighlight>좋음</StyledDustHighlight></StyledDustStatus>
-                </DustContentBottom>
-            </WeatherContent>
-            <Notice />
-        </ WeatherNoticeContainer>
-    )
-}
+  const [pm10Text] = convertPm10ToString(dust.pm10);
+  const [pm25Text] = convertPm25ToString(dust.pm25);
+
+  const background: Record<string, string> = {
+    CLEAR: 'linear-gradient(to top, #dbe4e6, #1c92d2)',
+    RAIN: 'linear-gradient(to top, #a8c0ff, #72a6d6)',
+    CLOUDS: 'linear-gradient(to bottom, #a5d2f7, #e9d9c6)',
+    MIST: 'linear-gradient(to top, #c7cdd8, #757F9A)',
+    HAZE: 'linear-gradient(to top, #c7cdd8, #757F9A)',
+    SNOW: 'linear-gradient(to top, #eef2f3, #8e9eab)'
+  };
+
+  const status: Record<string, string> = {
+    CLEAR: '맑음',
+    RAIN: '비',
+    CLOUDS: '구름',
+    MIST: '안개',
+    HAZE: '안개',
+    SNOW: '눈'
+  };
+
+  return (
+    <WeatherNoticeContainer>
+      <WeatherHeader>
+        <Heading2>오늘의 날씨</Heading2>
+      </WeatherHeader>
+      <WeatherContent background={background.CLOUDS}>
+        <WeatherContentTop>
+          <WeatherIcon weather="CLOUDS" />
+          <WeatherContentStatus>
+            <StyledWeatherTemp>{weather.temp}℃</StyledWeatherTemp>
+            <StyledWeatherStatus>{status[weather.status]}</StyledWeatherStatus>
+          </WeatherContentStatus>
+        </WeatherContentTop>
+        <DustContentBottom>
+          <StyledDustStatus>
+            미세먼지 <StyledDustHighlight>{pm10Text}</StyledDustHighlight>
+          </StyledDustStatus>
+          <StyledDustStatus>
+            초미세먼지 <StyledDustHighlight>{pm25Text}</StyledDustHighlight>
+          </StyledDustStatus>
+        </DustContentBottom>
+      </WeatherContent>
+      <Notice />
+    </WeatherNoticeContainer>
+  );
+};
 
 export default WeatherCard;
