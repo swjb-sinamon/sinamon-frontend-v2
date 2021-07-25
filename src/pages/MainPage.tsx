85c22e8c11a25b0b08b564ade2cfa02b0e55cce6
shc0801 <shc123987@naver.com>
2021-07-25 19:28:00 +0900

    fix: change WeatherCard path

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DefaultLayout from '../layouts/DefaultLayout';
-import WeatherCard from '../components/Card/WeatherCard';
+import WeatherCard from '../components/Weather';
 
 const MainPage: React.FC = () => {
   return (
