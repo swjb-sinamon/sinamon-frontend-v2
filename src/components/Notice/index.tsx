d409825b5bcd4bc4a18d811c53121c624d5271d0
shc0801 <shc123987@naver.com>
2021-07-25 19:25:33 +0900

    fix: update connect notice api and Prettier

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import Api from '../../apis';
 
 const NoticeContainer = styled.div`
-    font-size: .8rem;
-    color: #666;
+  font-size: .9rem;
+  color: #666;
 
-    display: flex;
-    justify-content: space-between;
+  display: flex;
 
-    margin-top: 8px;
-    padding: 0 5px;
-`
+  margin-top: 8px;
+  padding: 0 8px;
+`;
 
-const NoticeTitle = styled.span`
-    color: #111d68;
-`
+const NoticeTitle = styled.p`
+  width: 65px;
 
-const NoticeHighlight = styled.span`
-    color: #4a506b;
+  color: #111d68;
+  font-weight: bold;
+`;
+
+const NoticeContent = styled.span`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `
 
 const Notice: React.FC = () => {
-    return (
-        <NoticeContainer>
-            <NoticeTitle>코로나19 현황</NoticeTitle>
-            확진환자 <NoticeHighlight>치료중 157명</NoticeHighlight>
-            | 관리대상자 <NoticeHighlight>자가격리 753명</NoticeHighlight>
-        </NoticeContainer>
-    )
-}
+  const [notice, setNotice] = useState<string>('');
+
+  useEffect(() => {
+    Api.get('/notice').then((res) => {
+      setNotice(res.data.data);
+    });
+  }, []);
+
+  return (
+    <NoticeContainer>
+      <NoticeTitle>공지사항</NoticeTitle>
+      <NoticeContent>{notice}</NoticeContent>
+    </NoticeContainer>
+  );
+};
 
 export default Notice;
