4e3a5d235d9ed71290b1d486109dfeba1b7a6cdc
Gungoguma <khh9550@naver.com>
2021-08-03 15:26:55 +0900

    fix: meal layout

--- src/pages/MealPage.tsx
+++ src/pages/MealPage.tsx
@@ -1,74 +1,63 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import Side from '../components/Sidebar';
+import DefaultLayout from '../layouts/DefaultLayout';
 import { Heading2, Heading3 } from '../atoms/Typography/Heading';
 import { Gap } from '../utils/Gap';
-import { Breakpoints, makeMediaQuery } from '../styles/Breakpoint';
 import { useMeal } from '../hooks/useMeal';
 
-const Container = styled.div`
-    height: 100vh;
-
-    display: flex;
-    justify-content: center;
-
-`;
 
 const StyledHeading3 = styled(Heading3)`
   color: var(--color-main);
 `;
 
 const Box = styled.div`
-    padding: 32px;
-    width: 100vw;
+  padding: 32px;
+  width: 100%;
 `;
 
 const MealList = styled.div`
-    display: flex;
-    width: 70%;
-    justify-content: space-between;
+  display: flex;
+  width: 60vw;
+  justify-content: space-between;
+
+
 `;
 
 const MealBox = styled.div`
-    width: 25vw;
-    height: 26vh;
-    border-radius: 3px;
-    border: 1px solid var(--color-gray);
-    border-radius: 30px;
+  width: 25vw;
+  height: 300px;
+  border-radius: 3px;
+  border: 1px solid var(--color-gray);
+  border-radius: 30px;
 
-    padding: 2rem 2rem 2.5rem;
-    box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
-    ${makeMediaQuery(Breakpoints.MD)} {
-    display: block;
-  }
+  padding: 2rem 2rem 2.5rem;
+  box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
+
+
 `;
 
 const MealPage = () => {
-    const Meal = useMeal();
-    return (
-        <Container>
-            <Side />
-
-            <Box>
-                <Heading2>이번주 급식</Heading2>
-                <Gap gap={8} />
-                <MealList>
-                    <MealBox>
-                        <StyledHeading3>오늘</StyledHeading3>
-                        <Gap gap={16} />
-                        <p>{Meal}</p>
-                    </MealBox>
-                    <MealBox>
-                        <StyledHeading3>내일</StyledHeading3>
-                        <Gap gap={16} />
-                        <p>고기</p>
-                        <p>돼지고기</p>
-                        <p>소고기</p>
-                    </MealBox>
-                </MealList>
-            </Box>
-        </Container>
-    );
+  const { today, tomorrow } = useMeal();
+  return (
+    <DefaultLayout>
+        <Box>
+          <Heading2>이번주 급식</Heading2>
+          <Gap gap={8} />
+          <MealList>
+            <MealBox>
+              <StyledHeading3>오늘</StyledHeading3>
+              <Gap gap={16} />
+              <p>{today}</p>
+            </MealBox>
+            <MealBox>
+              <StyledHeading3>내일</StyledHeading3>
+              <Gap gap={16} />
+              <p>{tomorrow}</p>
+            </MealBox>
+          </MealList>
+        </Box>
+    </DefaultLayout>
+  );
 };
 
 export default MealPage;
