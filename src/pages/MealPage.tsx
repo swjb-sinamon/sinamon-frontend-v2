e65b94c785f9e1e307017cbbc153365cb91c71fb
Gungoguma <khh9550@naver.com>
2021-08-03 15:30:14 +0900

    feat: add mobile css

--- src/pages/MealPage.tsx
+++ src/pages/MealPage.tsx
@@ -4,6 +4,7 @@ import DefaultLayout from '../layouts/DefaultLayout';
 import { Heading2, Heading3 } from '../atoms/Typography/Heading';
 import { Gap } from '../utils/Gap';
 import { useMeal } from '../hooks/useMeal';
+import { Breakpoints, makeMediaQuery } from '../styles/Breakpoint';
 
 
 const StyledHeading3 = styled(Heading3)`
@@ -20,7 +21,12 @@ const MealList = styled.div`
   width: 60vw;
   justify-content: space-between;
 
-
+  ${makeMediaQuery(Breakpoints.MD)} {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: space-between;
+  }
 `;
 
 const MealBox = styled.div`
@@ -33,7 +39,15 @@ const MealBox = styled.div`
   padding: 2rem 2rem 2.5rem;
   box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
 
-
+  ${makeMediaQuery(Breakpoints.MD)} {
+    width: 100%;
+    height: 400px;
+    border-radius: 3px;
+    border: 1px solid var(--color-gray);
+    border-radius: 30px;
+    box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
+    margin-bottom: 1rem;
+  }
 `;
 
 const MealPage = () => {
