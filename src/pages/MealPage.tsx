ffcbaf6f7217c0ce1b2254134dda48e86173361f
DaegyeomHa <combbm@naver.com>
2021-08-11 18:08:20 +0900

    style: lint and design rule

--- src/pages/MealPage.tsx
+++ src/pages/MealPage.tsx
@@ -6,7 +6,6 @@ import { Gap } from '../utils/Gap';
 import { useMeal } from '../hooks/useMeal';
 import { Breakpoints, makeMediaQuery } from '../styles/Breakpoint';
 
-
 const StyledHeading3 = styled(Heading3)`
   color: var(--color-main);
 `;
@@ -17,7 +16,7 @@ const Box = styled.div`
 
 const MealList = styled.div`
   width: 53vw;
-  
+
   display: flex;
   justify-content: space-between;
 
@@ -25,7 +24,7 @@ const MealList = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    
+
     width: 100%;
   }
 `;
@@ -33,7 +32,7 @@ const MealList = styled.div`
 const MealBox = styled.div`
   width: 25.5vw;
   height: 300px;
-  
+
   border: 1px solid var(--color-gray);
   border-radius: 32px;
 
@@ -41,42 +40,46 @@ const MealBox = styled.div`
   box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
 
   transition: 0.3s;
-  &:hover{
+
+  &:hover {
     transform: translateY(-3px);
   }
 
   ${makeMediaQuery(Breakpoints.MD)} {
     width: 100%;
     height: 400px;
-   
+
     border: 1px solid var(--color-gray);
     border-radius: 32px;
-    
-    box-shadow: 0 0 30px rgba(169, 169, 169, 0.2); 
+
+    box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
     margin-bottom: 1rem;
   }
 `;
 
 const MealPage = () => {
   const { today, tomorrow } = useMeal();
+
   return (
     <DefaultLayout>
-        <Box>
-          <Heading2>급식</Heading2>
-          <Gap gap={24} />
-          <MealList>
-            <MealBox>
-              <StyledHeading3>오늘</StyledHeading3>
-              <Gap gap={32} />
-              <p>{today}</p>
-            </MealBox>
-            <MealBox>
-              <StyledHeading3>내일</StyledHeading3>
-              <Gap gap={16} />
-              <p>{tomorrow}</p>
-            </MealBox>
-          </MealList>
-        </Box>
+      <Heading2>급식</Heading2>
+
+      <Gap gap={32} />
+
+      <Box>
+        <MealList>
+          <MealBox>
+            <StyledHeading3>오늘</StyledHeading3>
+            <Gap gap={32} />
+            <p>{today}</p>
+          </MealBox>
+          <MealBox>
+            <StyledHeading3>내일</StyledHeading3>
+            <Gap gap={16} />
+            <p>{tomorrow}</p>
+          </MealBox>
+        </MealList>
+      </Box>
     </DefaultLayout>
   );
 };
