4507a86761b2ae61489e4a90dbc42783ddd38f1c
HARDTACK-Dev <rjsdn827@gmail.com>
2021-08-22 02:52:51 +0900

    feat: 유저 관리 페이지
    - Prettier 정렬 완료
    - Import 순서 변경 (for 가시성)
    
    -  미디어쿼리 적용
    - 주석 제거

--- src/pages/admin/AdminUserPage.tsx
+++ src/pages/admin/AdminUserPage.tsx
@@ -1,30 +1,76 @@
 import React, { useState, useEffect, useCallback } from 'react';
-// import styled from '@emotion/styled'
-import { Helmet } from 'react-helmet-async';
-import DefaultLayout from '../../layouts/DefaultLayout';
-import Api from '../../apis'
-import { ProfileType } from '../../types/ApiResponse';
-import Pagination from '../../components/Pagination';
 
+import styled from '@emotion/styled';
+import { Helmet } from 'react-helmet-async';
+import { ProfileType } from '../../types/ApiResponse';
+
+import { Gap } from '../../utils/Gap';
+import { Label } from '../../atoms/Form/Label';
 import { Input } from '../../atoms/Form/Input';
 import { Select } from '../../atoms/Form/Select';
-import { Label } from '../../atoms/Form/Label'
 import { Heading2 } from '../../atoms/Typography/Heading';
-import { Gap } from '../../utils/Gap';
+import { Breakpoints, makeMediaQuery } from '../../styles/Breakpoint';
+
+import Api from '../../apis';
 import Emoji from '../../atoms/Emoji';
+import UserTable from '../../components/User/UserTable';
+import Pagination from '../../components/Pagination';
+import DefaultLayout from '../../layouts/DefaultLayout';
 
-import  UserTable from '../../components/User/UserTable';
+const UserContainer = styled.div`
+  width: 100%;
+  
+`;
 
-// const ControlSection = styled.div`
-//   display: flex;
+const ControlContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  
+  input {
+    width: 23rem;
+    margin-top: 10px;
+  }
 
-//   input{ margin-right: 8px; }
-//   label{ margin-left: 12rem; }
-// `;
+  select {
+    width: 8rem;
+  }
+
+  ${makeMediaQuery(Breakpoints.LG)} {
+    flex-direction: column;
+
+    input {
+      width: 30rem;
+    }
+
+    select {
+      width: 14rem;
+    }
+  }
+
+  ${makeMediaQuery(Breakpoints.MD)} {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const SelectContainer = styled.div`
+  display: flex;
+  select {
+    margin-top: 10px;
+    margin-left: 10px;
+  }
+
+  ${makeMediaQuery(Breakpoints.MD)} {
+    justify-content: space-between;
+    select {
+      width: 12rem;
+    }
+  }
+`;
 
 const UserPage: React.FC = () => {
-  const [data, setData] = useState< ProfileType[]>([]);
-  
+  const [data, setData] = useState<ProfileType[]>([]);
+
   const [page, setPage] = useState<number>(1);
   const [count, setCount] = useState<number>(0);
 
@@ -33,22 +79,20 @@ const UserPage: React.FC = () => {
 
   const [search, setSearch] = useState<string>('');
 
-  const fetchData = useCallback( // p is for page (query)
-    (p: number, searchValue: string)=>{
+  const fetchData = useCallback(
+    (p: number, searchValue: string) => {
       const gradeQuery = grade ? `&filters[grade]=${grade}` : '';
       const clazzQuery = clazz ? `&filters[class]=${clazz}` : '';
-      console.log(`${grade  }-${  clazz}`)
-      Api.get(
-          `/auth/user?limit=30&offset=${p}&search=${searchValue}${gradeQuery}${clazzQuery}`
-        ).then((res) => {
-          setData(res.data.data);
-          setCount(res.data.count);
-          console.log(res.data.data)
-        });
-      }, [clazz ,grade]);
-     
 
-  useEffect(() => { 
+      Api.get(`/auth/user?limit=30&offset=${p}&search=${searchValue}${gradeQuery}${clazzQuery}`).then((res) => {
+        setData(res.data.data);
+        setCount(res.data.count);
+      });
+    },
+    [clazz, grade]
+  );
+
+  useEffect(() => {
     fetchData(1, '');
   }, [fetchData]);
 
@@ -59,64 +103,66 @@ const UserPage: React.FC = () => {
       </Helmet>
 
       <DefaultLayout isAdmin>
-        <Heading2>
-          <Emoji label="people" symbol="🧑‍🤝‍🧑" /> 유저 관리
-        </Heading2>
+        <UserContainer>
+          <Heading2>
+            <Emoji label="people" symbol="🧑‍🤝‍🧑" /> 유저 관리
+          </Heading2>
 
-        <Gap gap={32} />
+          <Gap gap={32} />
 
-       
-        <Input
-          type="text"
-          placeholder="사용자 이름 검색(엔터)"
-          onKeyPress={(e) =>{
-            if (e.key === 'Enter') fetchData(1, search)
-          }}
-          onChange={(e) => setSearch(e.target.value)}
+          <ControlContainer>
+            <Input
+              type="text"
+              placeholder="유저 이름 검색(엔터)"
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') fetchData(1, search);
+              }}
+              onChange={(e) => setSearch(e.target.value)}
               value={search}
-          />        
+            />
 
-          <Select
-            onChange={(e) => {
-              setGrade(e.target.value);
-              setSearch('');
-            }}
-            value={grade}
-          >
-            <option value="">학년 전체</option>
-            <option value="1">1학년</option>
-            <option value="2">2학년</option>
-            <option value="3">3학년</option>
-          </Select>
-            
+            <SelectContainer>
+              <Select
+                onChange={(e) => {
+                  setGrade(e.target.value);
+                  setSearch('');
+                }}
+                value={grade}
+              >
+                <option value="">학년 전체</option>
+                <option value="1">1학년</option>
+                <option value="2">2학년</option>
+                <option value="3">3학년</option>
+              </Select>
 
-          <Select
-            onChange={(e) => {
-              setclazz(e.target.value);
-              setSearch('');
-            }}
-            value={clazz}
-          >
-            <option value="">반 전체</option>
-            <option value="1">1반</option>
-            <option value="2">2반</option>
-          </Select>
-          
+              <Select
+                onChange={(e) => {
+                  setclazz(e.target.value);
+                  setSearch('');
+                }}
+                value={clazz}
+              >
+                <option value="">반 전체</option>
+                <option value="1">1반</option>
+                <option value="2">2반</option>
+              </Select>
+            </SelectContainer>
+          </ControlContainer>
+
+          <Gap gap={16} />
+
+          <UserTable data={data} />
+
+          <Gap gap={16} />
           <Label>전체: {count}명</Label>
-
-       
-
-
-          <Gap gap={16} />
-
-          <UserTable
-            data={data} 
+          <Pagination
+            onPageChange={(pageOffset) => fetchData(pageOffset, search)}
+            dataCount={count}
+            pageLimit={10}
+            page={page}
+            setPage={setPage}
           />
-
-          <Gap gap={16} />
-
-          <Pagination onPageChange={(pageOffset) => fetchData(pageOffset, search)} dataCount={count} pageLimit={10} page={page} setPage={setPage} />
-
+        </UserContainer>
       </DefaultLayout>
     </>
   );
