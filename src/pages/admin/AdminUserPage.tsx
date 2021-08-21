813d4750f05329cab762ba3edb0d96d4172df1de
HARDTACK-Dev <rjsdn827@gmail.com>
2021-08-21 19:23:10 +0900

    feat: 기능 구현 (1)
    Done
    - 사용자 이름으로 값 가져오기
    Remain
    - 학년 & 반별로 따로 가져오기

--- src/pages/admin/AdminUserPage.tsx
+++ src/pages/admin/AdminUserPage.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 // import styled from '@emotion/styled'
 import { Helmet } from 'react-helmet-async';
 import DefaultLayout from '../../layouts/DefaultLayout';
-// import Api from '../../apis'
-// import { ProfileType } from '../../types/ApiResponse';
+import Api from '../../apis'
+import { ProfileType } from '../../types/ApiResponse';
+import Pagination from '../../components/Pagination';
 
-// import { Input } from '../../atoms/Form/Input';
+import { Input } from '../../atoms/Form/Input';
+import { Select } from '../../atoms/Form/Select';
+import { Label } from '../../atoms/Form/Label'
 import { Heading2 } from '../../atoms/Typography/Heading';
+import { Gap } from '../../utils/Gap';
 import Emoji from '../../atoms/Emoji';
 
+import  UserTable from '../../components/User/UserTable';
+
 const UserPage: React.FC = () => {
-  // const [data, setData] = useState<ProfileType>([]);
-  // const [count, setCount] = useState<number>(0);
+  const [data, setData] = useState< ProfileType[]>([]);
+  
+  const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
 
-  // const [department, setDepartment] = useState<number>(0);
-  // const [Class, setClass] = useState<string>('');
+  const [grade, setGrade] = useState<string>('');
+  const [Class, setClass] = useState<string>('');
 
-  // const fetchData = useCallback(
-  //     (page: number, searchValue)=>{
-  //         const depQuery = department ? `&filters[department]=${department}` : '';
-  //         const gradeAndClass = Class ? Class.split('-') : [];
-  //         const gradeQuery = Class ? `&filters[grade]=${gradeAndClass[0]}` : '';
-  //         const classQuery = Class ? `&filters[Class]=${gradeAndClass[1]}` : '';
+  const [search, setSearch] = useState<string>('');
 
-  //         Api.get(
-  //             `/auth/user?limit=30&offset=${page}&search=${searchValue}${depQuery}${gradeQuery}${classQuery}`
-  //           ).then((res) => {
-  //             setData(res.data.data);
-  //             setCount(res.data.count);
-  //           });
-  //     },
-  //     [department, Class]
-  // );
+  const fetchData = useCallback( // p is for page (query)
+    (p: number, searchValue: string)=>{
+      const gradeQuery = grade ? `&filters[grade]=${grade}` : '';
+      const classQuery = Class ? `&filters[Class]=${Class}` : '';
 
-  // useEffect(() => {
-  //     fetchData(1, '');
-  //   }, [fetchData]);
+      Api.get(
+          `/auth/user?limit=30&offset=${p}&search=${searchValue}${gradeQuery}${classQuery}`
+        ).then((res) => {
+          setData(res.data.data);
+          setCount(res.data.count);
+        });
+      }, [Class ,grade]);
+     
+
+  useEffect(() => { fetchData(1, '');}, [fetchData]);
 
   return (
     <>
@@ -47,7 +52,52 @@ const UserPage: React.FC = () => {
         <Heading2>
           <Emoji label="people" symbol="🧑‍🤝‍🧑" /> 유저 관리
         </Heading2>
-        
+
+        <Gap gap={32} />
+
+        <Input
+          type="text"
+          placeholder="사용자 이름 검색(엔터)"
+          onKeyPress={(e) =>{
+            if (e.key === 'Enter') fetchData(1, search)
+          }}
+          onChange={(e) => setSearch(e.target.value)}
+              value={search}
+          />
+
+          <Select
+            onChange={(e) => {
+              setClass(e.target.value);
+              setSearch('');
+            }}
+            value={Class}
+          >
+            <option value="">학년 전체</option>
+            <option value="1">1학년</option>
+            <option value="2">2학년</option>
+            <option value="3">3학년</option>
+          </Select>
+
+          <Select
+            onChange={(e) => {
+              setGrade(e.target.value);
+              setSearch('');
+            }}
+            value={grade}
+          >
+            <option value="">반 전체</option>
+            <option value="1">1반</option>
+            <option value="2">2반</option>
+          </Select>
+              
+          <Label>전체: {count}명</Label>
+
+          <UserTable
+            data={data} 
+          />
+
+          <Pagination onPageChange={(pageOffset) => fetchData(pageOffset, search)} dataCount={count} pageLimit={10} page={page} setPage={setPage} />
+
       </DefaultLayout>
     </>
   );
