bd959c3c85dec2f75816481f227e34705fad3b69
DaegyeomHa <combbm@naver.com>
2021-08-30 20:13:01 +0900

    fix: 미세먼지 관련 코드 삭제

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,20 +1,15 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { DustType, WeatherType } from '../types/ApiResponse';
+import { WeatherType } from '../types/ApiResponse';
 import Api from '../apis';
 
 interface WeatherState {
   readonly weather: WeatherType;
-  readonly dust: DustType;
 }
 
 const DEFAULT_STATE: WeatherState = {
   weather: {
     status: 'NONE',
     temp: 0
-  },
-  dust: {
-    pm10: 0,
-    pm25: 0
   }
 };
 
@@ -22,22 +17,17 @@ const context = createContext<WeatherState>(DEFAULT_STATE);
 
 export const WeatherProvider: React.FC = ({ children }) => {
   const [weather, setWeather] = useState<WeatherType>(DEFAULT_STATE.weather);
-  const [dust, setDust] = useState<DustType>(DEFAULT_STATE.dust);
 
   useEffect(() => {
     Api.get('/weather').then((res) => {
       setWeather(res.data.data);
-    });
-    Api.get('/weather/dust').then((res) => {
-      setDust(res.data.data);
     });
   }, []);
 
   return (
     <context.Provider
       value={{
-        weather,
-        dust
+        weather
       }}
     >
       {children}
